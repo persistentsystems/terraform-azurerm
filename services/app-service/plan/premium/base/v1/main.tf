5dbb8430259b2234faf6833e074eda4881acd672
Mark Tinderholt <mark_tinderholt@persistent.com>
2020-07-15 06:06:23 -0400

    update app service plan

--- services/app-service/plan/premium/base/v1/main.tf
+++ services/app-service/plan/premium/base/v1/main.tf
@@ -1,33 +1,33 @@
 resource "azurerm_app_service_plan" "app_service_plan" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  name                = var.service_settings.name
+  location            = var.context.location
+  resource_group_name = var.context.resource_group_name
   kind                = "elastic"
 
   sku {
     tier = "ElasticPremium"
     size = "EP2"
-    capacity = var.minimum_instance_count
+    capacity = var.service_settings.minimum_instance_count
   }
 
-  maximum_elastic_worker_count = var.maximum_instance_count
+  maximum_elastic_worker_count = var.service_settings.maximum_instance_count
 
   tags = {
-    app = var.application_name
-    env = var.environment_name
+    app = var.context.application_name
+    env = var.context.environment_name
   }
 
 }
 
 module "func_storage" {
   
-  source                = "github.com/markti/tf_azure_storage/blob/account"
-  resource_group_name   = var.resource_group_name
-  location              = var.location
-  application_name              = var.application_name
-  environment_name              = var.environment_name
+  source                = "../../../../../services/storage/endpoint/base/v1"
+  resource_group_name   = var.context.resource_group_name
+  location              = var.context.location
+  application_name      = var.context.application_name
+  environment_name      = var.context.environment_name
   name                  = "func"
 
-  type                  = var.storage_type
+  type                  = var.service_settings.storage_type
 
 }
