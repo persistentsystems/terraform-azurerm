8dc817932969889bd9f42cb2ae05758b5cbe188f
Mark Tinderholt <mark_tinderholt@persistent.com>
2020-07-15 07:46:38 -0400

    fixed outputs for asp

--- services/app-service/plan/premium/base/v1/outputs.tf
+++ services/app-service/plan/premium/base/v1/outputs.tf
@@ -2,13 +2,13 @@ output "id" {
     value = azurerm_app_service_plan.app_service_plan.id
 }
 output "storage_connection_string" {
-    value = 
+    value = module.func_storage.primary_connection_string
 }
 output "storage_account_name" {
-    value = module.func_storage.primary_connection_string
+    value = module.func_storage.name
 }
 output "storage_account_access_key" {
-    value = module.func_storage.primary_connection_string
+    value = module.func_storage.primary_access_key
 }
 output "storage_account" {
   value = {
