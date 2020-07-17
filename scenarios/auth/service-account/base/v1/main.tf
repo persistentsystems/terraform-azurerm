1501a451663f634759239b2d7bb49ac779c45bab
Mark Tinderholt <mark_tinderholt@persistent.com>
2020-07-16 22:17:35 -0400

    app/graph-read

--- scenarios/auth/service-account/base/v1/main.tf
+++ scenarios/auth/service-account/base/v1/main.tf
@@ -1,6 +1,6 @@
 module "app" {
   
-  source  = "../../../../../services/application/graph-read/v1"
+  source  = "../../../../../../services/application/graph-read/v1"
   name    = var.name
   identifier_uris  = [ var.application_id_uri ] 
 
