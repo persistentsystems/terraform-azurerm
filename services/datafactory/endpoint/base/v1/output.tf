e1c21d5335a8d4a8c98ce06256cef3d201713f23
Supratim Mukherjee <supratim_mukherjee@persistent.co.in>
2020-07-17 15:12:55 -0400

    Added output.tf to datafactory module to export id

---
+++ services/datafactory/endpoint/base/v1/output.tf
@@ -0,0 +1,9 @@
+output "ADF_ID" {
+  value       = azurerm_data_factory.datafactory.id
+  description = "The generated ID for the ADF created"
+}
+
+output "ADF_NAME" {
+  value       = azurerm_data_factory.datafactory.name
+  description = "The generated ID for the ADF created"
+}
