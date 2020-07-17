482ae6efe83fd9fb128c7665438d2b384a4b8d21
Mark Tinderholt <mark_tinderholt@persistent.com>
2020-07-16 22:04:40 -0400

    correct relative path

--- scenarios/auth/service-account/base/v1/main.tf
+++ scenarios/auth/service-account/base/v1/main.tf
@@ -1,13 +1,13 @@
 module "app" {
   
-  source  = "../../services/application/graph-read/v1"
+  source  = "../../../../../services/application/graph-read/v1"
   name    = var.name
   identifier_uris  = [ var.application_id_uri ] 
 
 }
 module "sp" {
   
-  source          = "../../services/service-principal/base/v1"
+  source          = "../../../../../services/service-principal/base/v1"
   application_id  = module.app.application_id
 
 }
