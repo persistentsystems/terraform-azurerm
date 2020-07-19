7e1cfa4f0a5dbb547c5674d502e61909726c59c6
Mark Tinderholt <mark_tinderholt@persistent.com>
2020-07-19 19:27:38 -0400

    add principal id

--- services/managed-identity/base/v1/outputs.tf
+++ services/managed-identity/base/v1/outputs.tf
@@ -5,5 +5,5 @@ output "id" {
   value = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourcegroups/${var.context.resource_group_name}/providers/Microsoft.ManagedIdentity/userAssignedIdentities/${var.service_settings.name}"
 }
 output "client_id" {
-  value = "${lookup(azurerm_template_deployment.managed_identity_rgt.outputs, "principalId")}"
+  value = azurerm_template_deployment.managed_identity_rgt.outputs["principalId"]
 }
