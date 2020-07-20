91a641d21c4fc3810a3c99f0ee12043279e50df1
Mark Tinderholt <mark_tinderholt@persistent.com>
2020-07-20 10:53:12 -0400

    apim with logging

---
+++ services/api-management/endpoint/base/v1.1/diagnostics.tf
@@ -0,0 +1,49 @@
+
+resource "azurerm_monitor_diagnostic_setting" "frontdoor_diagnostic_setting" {
+
+  name                        = "${var.service_settings.name}-apim-logs"
+  target_resource_id          = azurerm_frontdoor.frontdoor.id
+  log_analytics_workspace_id  = var.observability_settings.workspace_id
+
+  log {
+    category = "GatewayLogs"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+    }
+  }
+
+  metric {
+    category = "Gateway Requests"
+
+    retention_policy {
+      enabled = true
+    }
+  }
+
+  metric {
+    category = "Capacity"
+
+    retention_policy {
+      enabled = true
+    }
+  }
+
+  metric {
+    category = "EventHub Events"
+
+    retention_policy {
+      enabled = true
+    }
+  }
+
+  metric {
+    category = "Network Status"
+
+    retention_policy {
+      enabled = true
+    }
+  }
+
+}
