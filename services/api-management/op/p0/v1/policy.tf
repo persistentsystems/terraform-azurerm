261ffd586072b339045a42434fa65997a4542319
Mark Tinderholt <mark_tinderholt@persistent.com>
2020-07-20 04:23:25 -0400

    update op

--- services/api-management/op/p0/v1/policy.tf
+++ services/api-management/op/p0/v1/policy.tf
@@ -1,7 +1,7 @@
 resource "azurerm_api_management_api_operation_policy" "op_policy" {
-  api_name            = var.api_name
-  api_management_name = var.apim_name
-  resource_group_name = var.resource_group_name
+  api_name            = var.service_settings.api_name
+  api_management_name = var.service_settings.endpoint_name
+  resource_group_name = var.context.resource_group_name
   operation_id        = azurerm_api_management_api_operation.op.operation_id
 
   xml_content = <<XML
--- services/api-management/op/p4/v1/main.tf
+++ services/api-management/op/p4/v1/main.tf
@@ -1,12 +1,15 @@
 resource "azurerm_api_management_api_operation" "op" {
-  operation_id        = var.operation_id
-  api_name            = var.api_name
-  api_management_name = var.apim_name
-  resource_group_name = var.resource_group_name
-  display_name        = var.display_name
-  description         = var.description
-  method              = var.method
-  url_template        = var.url_template
+  
+  resource_group_name = var.context.resource_group_name
+
+  api_name            = var.service_settings.api_name
+  api_management_name = var.service_settings.endpoint_name
+
+  operation_id        = var.operation_settings.operation_id
+  display_name        = var.operation_settings.display_name
+  description         = var.operation_settings.description
+  method              = var.operation_settings.method
+  url_template        = var.operation_settings.url_template
 
   response {
     status_code = 200
