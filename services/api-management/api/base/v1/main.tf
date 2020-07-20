5835b7e6f40429b8f84411e3b4d3a750fd374092
Mark Tinderholt <mark_tinderholt@persistent.com>
2020-07-20 02:31:38 -0400

    update var.service_settings

--- services/api-management/api/base/v1/main.tf
+++ services/api-management/api/base/v1/main.tf
@@ -4,12 +4,12 @@ resource "azurerm_api_management_api" "api" {
 
   resource_group_name = var.context.resource_group_name
 
-  api_management_name = var.service_setting.endpoint_name
-  
-  revision            = var.service_setting.revision
-  name                = var.service_setting.name
-  display_name        = var.service_setting.description
-  path                = var.service_setting.path
-  protocols           = [var.service_setting.primary_protocol]
+  api_management_name = var.service_settings.endpoint_name
+
+  revision            = var.service_settings.revision
+  name                = var.service_settings.name
+  display_name        = var.service_settings.description
+  path                = var.service_settings.path
+  protocols           = [var.service_settings.primary_protocol]
 
 }
