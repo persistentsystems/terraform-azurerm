873b635b09bb337c3bb0fc8e2a9d62b80bbc8e41
Mark Tinderholt <mark_tinderholt@persistent.com>
2020-07-19 21:30:13 -0400

    fix object_id var

--- services/keyvault/accesspolicy/templates/managed-identity-reader/v1/main.tf
+++ services/keyvault/accesspolicy/templates/managed-identity-reader/v1/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_key_vault_access_policy" "policy" {
   key_vault_id = var.keyvault_id
 
   tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = var.application_id
+  object_id = var.object_id
 
   key_permissions = [
     "get",
