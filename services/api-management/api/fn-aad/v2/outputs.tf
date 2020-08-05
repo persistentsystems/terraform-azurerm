8fd337b79e62eb1394b2563521cd9d9a855c2fde
supratim-persistent <68080807+supratim-persistent@users.noreply.github.com>
2020-08-05 18:52:05 -0400

    Added v2 for multiple backends

---
+++ services/api-management/api/fn-aad/v2/outputs.tf
@@ -0,0 +1,9 @@
+output "api_name" {
+    value = module.api.api_name
+}
+output "backend1_name" {
+    value = module.api.backend1_name
+}
+output "backend2_name" {
+    value = module.api.backend2_name
+}
