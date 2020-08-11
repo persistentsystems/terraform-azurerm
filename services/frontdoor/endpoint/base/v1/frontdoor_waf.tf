50069218f0d6ae3b5b25c01ce9e7cc89422b66d4
Aditya Pavan <aditya_pavan@persistent.co.in>
2020-08-11 23:48:03 +0530

    updated name settings

--- services/frontdoor/endpoint/base/v1/frontdoor_waf.tf
+++ services/frontdoor/endpoint/base/v1/frontdoor_waf.tf
@@ -1,11 +1,11 @@
 resource "azurerm_frontdoor_firewall_policy" "waf" {
-  name                              = "${var.context.application_name}-${var.context.environment_name}-wafpolicy"
+  name                              = "${var.service_settings.name}-wafpolicy"
   resource_group_name               = var.context.resource_group_name
   enabled                           = true
   mode                              = "Prevention"
   redirect_url                      = "https://www.example.com"
   custom_block_response_status_code = 403
-  custom_block_response_body        = "PGh0bWw+CjxoZWFkZXI+PHRpdGxlPkhlbGxvPC90aXRsZT48L2hlYWRlcj4KPGJvZHk+CkhlbGxvIHdvcmxkCjwvYm9keT4KPC9odG1sPg=="
+  custom_block_response_body        = ""
 
   managed_rule {
       type    = "DefaultRuleSet"
