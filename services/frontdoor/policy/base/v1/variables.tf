b04fc33cf7ddcd4cd9bf59a088810b813ff5d8c3
Aditya Pavan <aditya_pavan@persistent.co.in>
2020-08-12 17:22:06 +0530

    updated variable type to object

--- services/frontdoor/policy/base/v1/variables.tf
+++ services/frontdoor/policy/base/v1/variables.tf
@@ -28,26 +28,32 @@ variable "policy_settings" {
 }
 
 variable "managed_rule_set" {
-    managed_rule {
-      type    = string
-      version = string
-    }
+
+    type = object({
+        
+        managed_rule {
+            type    = string
+            version = string
+        }
+    })
 }
 
 variable "custom_rule_set" {
+    type = object({
 
-    custom_rule {
-        name        = string
-        enabled     = bool
-        priority    = number
-        type        = string
-        action      = string
+        custom_rule {
+            name        = string
+            enabled     = bool
+            priority    = number
+            type        = string
+            action      = string
 
-        match_condition {
-            match_variable     = string
-            operator           = string
-            negation_condition = bool
-            match_values       = string
-    }
-  }
+            match_condition {
+                match_variable     = string
+                operator           = string
+                negation_condition = bool
+                match_values       = string
+            }
+        }
+    })
 }
