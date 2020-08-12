ac61d6103f48caca7c84955614435f39c07e61bb
Aditya Pavan <aditya_pavan@persistent.co.in>
2020-08-12 14:41:31 +0530

    updated input variables

--- services/frontdoor/policy/base/v1/variables.tf
+++ services/frontdoor/policy/base/v1/variables.tf
@@ -1,8 +1,8 @@
 variable "context" {
   type = object({
 
-    application_name            = string
-    environment_name            = string
+    application_name    = string
+    environment_name    = string
     resource_group_name = string
     location            = string
     location_suffix     = string
@@ -12,8 +12,39 @@ variable "context" {
 variable "service_settings" {
   type = object({
 
-    name         = string
-    workspace_id = string
+    name  = string
 
   })
+}
+
+variable "policy_settings" {
+    type = object({
+        custom_block_response_status_code = number 
+        custom_block_response_body = string 
+        mode = string
+        redirect_url = string
+
+    })
+}
+
+variable "managed_rule_set" {
+    managed_rule {
+      type    = string
+      version = string
+    }
+}
+
+variable "custom_rule_set" {
+    custom_rule {
+        name = string
+        enabled = bool
+        priority = number
+
+        match_condition {
+            match_variable     = string
+            operator           = string
+            negation_condition = bool
+            match_values       = string
+    }
+  }
 }
