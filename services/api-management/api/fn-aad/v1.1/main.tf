a29d8ac5e138b34a66d3ae73f788d95e93272e89
Supratim Mukherjee <supratim_mukherjee@persistent.co.in>
2020-08-12 08:45:11 -0400

    Adding v1.1 under fn-aad for products mapping

---
+++ services/api-management/api/fn-aad/v1.1/main.tf
@@ -0,0 +1,37 @@
+data "azurerm_client_config" "current" {}
+
+locals {
+
+  jwt_authentication = <<XML
+    <validate-jwt header-name="Authorization" failed-validation-httpcode="401" failed-validation-error-message="Unauthorized. Access token is missing or invalid.">
+      <openid-config url="https://login.microsoftonline.com/${data.azurerm_client_config.current.tenant_id}/.well-known/openid-configuration" />
+      <required-claims>
+          <claim name="aud">
+              <value>${var.aad_settings.scope}</value>
+          </claim>
+      </required-claims>
+    </validate-jwt>
+XML
+
+  merged_policies = {
+
+    inbound          = "${var.policies.inbound} \n ${local.jwt_authentication}"
+    outbound         = var.policies.outbound
+    backend          = var.policies.backend
+    error            = var.policies.error
+
+  }
+
+}
+
+
+module "api" {
+  
+  source                = "../../fn/v1.1"
+  
+  context          = var.context
+  service_settings = var.service_settings
+  policies         = local.merged_policies
+  backend_settings = var.backend_settings
+
+}
---
+++ services/api-management/api/fn-aad/v1.1/outputs.tf
@@ -0,0 +1,6 @@
+output "api_name" {
+    value = module.api.api_name
+}
+output "backend_name" {
+    value = module.api.backend_name
+}
