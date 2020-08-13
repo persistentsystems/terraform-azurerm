1c6a1ac1826379a15061305d1339dddb014cc72d
Aditya Pavan <aditya_pavan@persistent.co.in>
2020-08-14 01:39:19 +0530

    arrey of rules

--- services/frontdoor/policy/base/v1/main.tf
+++ services/frontdoor/policy/base/v1/main.tf
@@ -4,26 +4,10 @@ resource "azurerm_frontdoor_firewall_policy" "waf" {
   enabled                           = true
 
   managed_rule { 
-        type    = var.managed_rules_settings.type
-        version = var.managed_rules_settings.version
+        type    = [var.managed_rules_settings.type]
+        version = [var.managed_rules_settings.version]
       }
   
-  #custom_rule {
-      #name                           = var.custom_rules_settings.name
-      #enabled                        = var.custom_rules_settings.enabled
-      #priority                       = var.custom_rules_settings.priority
-      #type                           = var.custom_rules_settings.type
-      #action                         = var.custom_rules_settings.action
-      #match_variable                 = var.custom_rules_settings.match_variable
-      #operator                       = var.custom_rules_settings.operator
-      #negation_condition             = var.custom_rules_settings.negation_condition
-      #match_values                   = var.custom_rules_settings.match_values
-      #rate_limit_duration_in_minutes = var.custom_rules_settings.rate_limit_duration_in_minutes
-      #rate_limit_threshold           = var.custom_rules_settings.rate_limit_threshold
-      #selector                       = var.custom_rules_settings.selector
-      #trasnforms                     = var.custom_rules_settings.transforms
-  #}
-
   tags = {
     app = var.context.application_name
     env = var.context.environment_name
