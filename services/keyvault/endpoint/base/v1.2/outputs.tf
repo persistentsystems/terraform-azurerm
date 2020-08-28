c41e2ab4864375016709e8b2a7f3616ce173e259
Mark Tinderholt <mark@vitalsigyn.com>
2020-08-27 23:59:38 -0400

    update observability settings

---
+++ services/keyvault/endpoint/base/v1.2/outputs.tf
@@ -0,0 +1,6 @@
+output "id" {
+  value = azurerm_key_vault.keyvault.id
+}
+output "uri" {
+  value = azurerm_key_vault.keyvault.vault_uri
+}
---
+++ services/keyvault/endpoint/base/v1.2/variables.tf
@@ -0,0 +1,32 @@
+  
+variable "context" {
+  type = object({
+
+    application_name = string
+    environment_name = string
+    resource_group_name = string
+    location = string
+    location_suffix = string
+
+  })
+}
+
+variable "service_settings" {
+  type = object({
+
+    name                = string
+    soft_delete_enabled = bool
+    log_storage_account = string 
+
+  })
+}
+
+variable "observability_settings" {
+  type = object({
+
+    instrumentation_key = string
+    workspace_id        = string
+    storage_account     = string 
+    retention_in_days   = number
+  })
+}
