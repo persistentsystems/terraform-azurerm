ca909e44f3ad1a759a6930dc1976d8efa20c6765
Santosh-Peddi <69802115+Santosh-Peddi@users.noreply.github.com>
2020-08-28 23:33:07 +0530

    Update variables.tf

--- services/frontdoor/endpoint/base/v1.1/variables.tf
+++ services/frontdoor/endpoint/base/v1.1/variables.tf
@@ -50,7 +50,6 @@ variable "backend_settings" {
 variable "observability_settings" {
   type = object({
 
-    instrumentation_key = string
     workspace_id        = string
     storage_account     = string 
     retention_days      = number
