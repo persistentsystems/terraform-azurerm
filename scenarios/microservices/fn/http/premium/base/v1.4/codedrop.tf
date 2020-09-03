c8e66a672bfcc7af2d474d2dcac9c91a81560e5d
supratim-persistent <68080807+supratim-persistent@users.noreply.github.com>
2020-09-03 15:47:07 -0400

    Create codedrop.tf

---
+++ scenarios/microservices/fn/http/premium/base/v1.4/codedrop.tf
@@ -0,0 +1,14 @@
+locals {
+
+  code_drop_prefix = "https://${var.host_settings.deployment_storage_account_name}.blob.core.windows.net/${var.host_settings.deployment_storage_container}"
+  code_drop_url = "${local.code_drop_prefix}/${azurerm_storage_blob.deployment_blob.name}${var.host_settings.deployment_package_sas}"
+
+}
+
+resource "azurerm_storage_blob" "deployment_blob" {
+  name                    = "${var.service_settings.name}.zip"
+  storage_account_name    = var.host_settings.deployment_storage_account_name
+  storage_container_name  = var.host_settings.deployment_storage_container
+  type                    = "Block"
+  source                  = var.service_settings.package_filename
+}
