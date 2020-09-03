ddde46f128f64943fdceed829284d71ccca4b3f6
Santosh Peddi <santosh_peddi@persistent.co.in>
2020-09-03 11:32:32 +0530

    Updated custom_rule

--- services/frontdoor/firewallPolicy/base/v1/main.tf
+++ services/frontdoor/firewallPolicy/base/v1/main.tf
@@ -23,19 +23,19 @@ resource "azurerm_frontdoor_firewall_policy" "waf" {
     for_each = var.custom_rules_settings
 
     content {
-      name                           = var.custom_rule.value.name
-      enabled                        = var.custom_rule.value.enabled
-      priority                       = var.custom_rule.value.priority
-      rate_limit_duration_in_minutes = var.custom_rule.value.rate_limit_duration_in_minutes
-      rate_limit_threshold           = var.custom_rule.value.rate_limit_threshold
-      type                           = var.custom_rule.value.type
-      action                         = var.custom_rule.value.action
+      name                           = custom_rule.value.name
+      enabled                        = custom_rule.value.enabled
+      priority                       = custom_rule.value.priority
+      rate_limit_duration_in_minutes = custom_rule.value.rate_limit_duration_in_minutes
+      rate_limit_threshold           = custom_rule.value.rate_limit_threshold
+      type                           = custom_rule.value.type
+      action                         = custom_rule.value.action
 
       match_condition {
-          match_variable             = var.custom_rule.value.match_variable
-          operator                   = var.custom_rule.value.operator
-          negation_condition         = var.custom_rule.value.negation_condition
-          match_values               = var.custom_rule.value.match_values
+          match_variable             = custom_rule.value.match_variable
+          operator                   = custom_rule.value.operator
+          negation_condition         = custom_rule.value.negation_condition
+          match_values               = custom_rule.value.match_values
       } 
     }
   }
