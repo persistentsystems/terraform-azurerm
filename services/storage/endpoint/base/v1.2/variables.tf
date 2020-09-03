e6d1b102e0c20e63d84122f1f956b140239cdc7e
Santosh Peddi <santosh_peddi@persistent.co.in>
2020-09-03 14:02:32 +0530

    added new version for storage account

---
+++ services/storage/endpoint/base/v1.2/variables.tf
@@ -0,0 +1,27 @@
+variable "context" {
+  type = object({
+
+    application_name    = string
+    environment_name    = string
+    resource_group_name = string
+    location            = string
+    location_suffix     = string
+
+  })
+}
+# tier = "Standard"
+# type = "GRS"
+variable "service_settings" {
+  type = object({
+
+    name            = string
+    tier            = string
+    type            = string
+
+  })
+}
+
+variable "security_settings" {
+  type = object({
+    allow_blob_public_access    = bool
+  })
