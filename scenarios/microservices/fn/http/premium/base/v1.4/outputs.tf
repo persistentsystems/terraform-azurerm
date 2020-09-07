9df0e949d479fc58d5a357592f331d04bcee2ce3
Supratim Mukherjee <supratim_mukherjee@persistent.co.in>
2020-09-07 17:05:58 -0400

    New v1.4 added for IP Whitelisting

---
+++ scenarios/microservices/fn/http/premium/base/v1.4/outputs.tf
@@ -0,0 +1,12 @@
+output "function_name" {
+    value = module.api_fn.name
+}
+output "function_key" {
+    value = module.api_fn.function_key
+}
+output "identity" {
+    value = module.api_fn.identity
+}
+output "hostname" {
+    value = module.api_fn.hostname
+}
