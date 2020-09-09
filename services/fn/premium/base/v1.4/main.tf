211dbe4fa118daa01d4ca3cbf13c3b99306c7029
Supratim Mukherjee <supratim_mukherjee@persistent.co.in>
2020-09-09 18:01:11 -0400

    Removing whitelist enabled check

--- services/fn/premium/base/v1.4/main.tf
+++ services/fn/premium/base/v1.4/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_function_app" "function_app" {
     pre_warmed_instance_count = 1
     dynamic "ip_restriction" {
         
-      for_each = var.service_settings.ip_whitelist_enabled ? local.ip_list : []
+      for_each = local.ip_list
               
         content {
             ip_address = ip_restriction.value.ip_address
