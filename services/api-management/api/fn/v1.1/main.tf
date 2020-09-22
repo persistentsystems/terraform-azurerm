5c2ad81fd1169f1ec4bb3d6ba21e0015808055a6
Aditya Pavan <aditya.pavan@fdbhealth.com>
2020-09-22 15:19:44 +0530

    enabled api logging #4153

--- services/api-management/api/fn/v1.1/main.tf
+++ services/api-management/api/fn/v1.1/main.tf
@@ -37,20 +37,3 @@ module "backend" {
   }
 
 }
-
-resource "azurerm_api_management_api_diagnostic" "api" {
-  resource_group_name      = var.context.resource_group_name
-  api_management_name      = azurerm_api_management.apim.name
-  api_name                 = module.api.name
-  api_management_logger_id = azurerm_api_management_logger.loggerid
-}
-
-resource "azurerm_api_management_logger" "app_insights" {
-  name                = "${var.service_settings.name}-appinsights-logger"
-  api_management_name = azurerm_api_management.apim.name
-  resource_group_name = var.context.resource_group_name
-
-  application_insights {
-    instrumentation_key = var.observability_settings.instrumentation_key
-  }
-}
