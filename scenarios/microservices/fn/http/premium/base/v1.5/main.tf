b087d0e75b08d270d8a831116665976ce345cafb
Santosh-Peddi <69802115+Santosh-Peddi@users.noreply.github.com>
2020-09-23 12:47:29 +0530

    Reverted changes related to US4151

--- scenarios/microservices/fn/http/premium/base/v1.5/main.tf
+++ scenarios/microservices/fn/http/premium/base/v1.5/main.tf
@@ -14,7 +14,7 @@ locals {
     app_settings        = local.combined_settings
     plan_id             = var.host_settings.plan_id
     storage_account     = var.host_settings.storage_account
-    storage_account_id  = var.observability_settings.storage_account_id 
+    storage_account_id  = var.observability_settings.storage_account_id
   }
 }
 
