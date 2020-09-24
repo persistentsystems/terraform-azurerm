78e26217803cad0cf9db009af62b1e211164e4fb
Santosh-Peddi <69802115+Santosh-Peddi@users.noreply.github.com>
2020-09-24 11:28:25 +0530

    Added output var for api

--- services/api-management/api/fn-aad/v1.1/outputs.tf
+++ services/api-management/api/fn-aad/v1.1/outputs.tf
@@ -4,3 +4,9 @@ output "api_name" {
 output "backend_name" {
     value = module.api.backend_name
 }
+output "api_description" {
+    value = module.api.service_settings.description
+}
+output "api_endpoint_name" {
+    value = module.api.service_settings.endpoint_name
+}
