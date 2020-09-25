7608b5f5da5fa17493ebd9440c3c422953fda305
Santosh-Peddi <69802115+Santosh-Peddi@users.noreply.github.com>
2020-09-25 15:17:30 +0530

    Added outputs for all required modules

--- services/api-management/api/fn/v1.1/outputs.tf
+++ services/api-management/api/fn/v1.1/outputs.tf
@@ -5,16 +5,15 @@ output "backend_name" {
     value = module.backend.name
 }
 
-/*
+
 output "description" {
-    value = module.api.service_settings.display_name
+    value = module.api.display_name
 }
 
 output "endpoint_name" {
-    value = module.api.service_settings.endpoint_name
+    value = module.api.endpoint_name
 }
 
 output "product_id" {
-    value = module.api.service_settings.product_id
+    value = module.api.product_id
 }
-*/
