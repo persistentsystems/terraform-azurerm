59aa59099d56f0dcc249dfdbaf3082ca02d5991c
Santosh-Peddi <69802115+Santosh-Peddi@users.noreply.github.com>
2020-09-25 16:43:25 +0530

    Reverted all changes and added only api_displayname

--- services/api-management/api/fn-aad/v1.1/outputs.tf
+++ services/api-management/api/fn-aad/v1.1/outputs.tf
@@ -8,11 +8,3 @@ output "backend_name" {
 output "api_displayname" {
     value = module.api.api_displayname
 }
-
-output "endpoint_name" {
-    value = module.api.endpoint_name
-}
-
-output "product_id" {
-    value = module.api.product_id
-}
--- services/api-management/api/fn/v1.1/outputs.tf
+++ services/api-management/api/fn/v1.1/outputs.tf
@@ -5,15 +5,6 @@ output "backend_name" {
     value = module.backend.name
 }
 
-
 output "api_displayname" {
     value = module.api.api_displayname
-}
-
-output "endpoint_name" {
-    value = module.api.service_settings.endpoint_name
-}
-
-output "product_id" {
-    value = module.api.service_settings.product_id
 }
