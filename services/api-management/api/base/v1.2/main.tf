5c7176388ad1e6d1e2936322ae0437ee18e9ff9a
Aditya Pavan <aditya.pavan@fdbhealth.com>
2020-09-30 15:43:37 +0530

    added identifier filed to api diagnostics #4153

--- services/api-management/api/base/v1.2/main.tf
+++ services/api-management/api/base/v1.2/main.tf
@@ -16,6 +16,7 @@ resource "azurerm_api_management_api" "api" {
 
 resource "azurerm_api_management_api_diagnostic" "api" {
   
+  identifier               = "applicationinsights"
   resource_group_name      = var.context.resource_group_name
   api_management_name      = var.service_settings.endpoint_name
   api_name                 = azurerm_api_management_api.api.name
