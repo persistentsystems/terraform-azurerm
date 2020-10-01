48cae139308ef33f29e2e4adcdf0a18e7df2c2bd
Aditya Pavan <aditya.pavan@fdbhealth.com>
2020-10-01 13:30:06 +0530

    added new version of cosmosdb #4658

---
+++ services/cosmos-db/database/sql/base/v1.1/variables.tf
@@ -0,0 +1,21 @@
+
+variable "context" {
+  type = object({
+
+    application_name    = string
+    environment_name    = string
+    resource_group_name = string
+    location            = string
+    location_suffix     = string
+
+  })
+}
+variable "service_settings" {
+  type = object({
+
+    name              = string
+    account_name      = string
+    throughput        = string
+
+  })
+}
