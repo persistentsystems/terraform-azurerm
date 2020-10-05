5b02fac4e6f5db1c3dd2006b2f37a8203cf4ab08
Aditya Pavan <aditya.pavan@fdbhealth.com>
2020-10-05 20:26:59 +0530

    #4658 Cosmosdb Container creation

--- services/cosmos-db/database/sql/base/v1.1/main.tf
+++ services/cosmos-db/database/sql/base/v1.1/main.tf
@@ -9,25 +9,12 @@ resource "azurerm_cosmosdb_sql_database" "database" {
 
 resource "azurerm_cosmosdb_sql_container" "erx" {
 
-
-  
-  
-  dynamic "container" {
-    
-    for_each = var.container
-
-    content {
-          name                = container.value.name
-          partition_key_path  = container.value.partition_key_path
-          throughput          = var.service_settings.throughput
-          resource_group_name = var.context.resource_group_name
-          account_name        = var.service_settings.account_name
-          database_name       = azurerm_cosmosdb_sql_database.database.name
-
-    }
-
-
-  }
+  throughput          = var.service_settings.throughput
+  resource_group_name = var.context.resource_group_name
+  account_name        = var.service_settings.account_name
+  database_name       = azurerm_cosmosdb_sql_database.database.name
+  name                = var.container.value.name
+  partition_key_path  = var.container.partition_key_path
 
   indexing_policy {
     indexing_mode = "Consistent"
