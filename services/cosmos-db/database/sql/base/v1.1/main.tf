a9a7cc3ae32577d08e1336940f10829aee5f9b0e
Aditya Pavan <aditya.pavan@fdbhealth.com>
2020-10-05 15:34:16 +0530

    #4658 Cosmosdb Container creation

--- services/cosmos-db/database/sql/base/v1.1/main.tf
+++ services/cosmos-db/database/sql/base/v1.1/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_cosmosdb_sql_container" "erx" {
           partition_key_path  = var.container.partition_key_path
           throughput          = var.service_settings.throughput
           resource_group_name = var.context.resource_group_name
-          account_name        = var.service_settings.account_name
+          account_name        = var.container.account_name
           database_name       = azurerm_cosmosdb_sql_database.database.name
 
     }
