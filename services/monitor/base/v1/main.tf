adf70a021f17ec0c57861e1c0c4e3661952b0cbd
Santosh-Peddi <69802115+Santosh-Peddi@users.noreply.github.com>
2020-10-05 15:27:19 +0530

    Changes as per US3541

--- services/monitor/base/v1/main.tf
+++ services/monitor/base/v1/main.tf
@@ -9,8 +9,9 @@ resource "azurerm_monitor_action_group" "monitor" {
     name          = "sendtoadmin"
     email_address = "testing@test.com"
   }
-}
 */
+}
+
 
 /*
 resource "azurerm_monitor_metric_alert" "alert" {
