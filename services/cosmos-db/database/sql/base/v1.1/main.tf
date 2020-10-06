13f1e542aa5eef61ce6b16b6f1be08d65428d34f
Aditya Pavan <aditya.pavan@fdbhealth.com>
2020-10-06 11:13:10 +0530

    #4658 Cosmosdb Container creation

--- services/cosmos-db/database/sql/base/v1.1/main.tf
+++ services/cosmos-db/database/sql/base/v1.1/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_cosmosdb_sql_database" "database" {
   name                = var.service_settings.name
   resource_group_name = var.context.resource_group_name
   account_name        = var.service_settings.account_name
-  throughput          = var.service_settings.throughput
+  #throughput          = var.service_settings.throughput
   autoscale_settings {
     max_throughput      = var.service_settings.max_throughput
   }  
