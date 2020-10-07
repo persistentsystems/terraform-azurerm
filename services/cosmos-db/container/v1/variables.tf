f9de1a9023abffc5c44b4741e7cb32d3bf913d2a
Aditya Pavan <aditya.pavan@fdbhealth.com>
2020-10-07 11:38:32 +0530

    #4658 Cosmosdb Container creation

--- services/cosmos-db/container/v1/variables.tf
+++ services/cosmos-db/container/v1/variables.tf
@@ -13,10 +13,7 @@ variable "context" {
 variable "service_settings" {
   type = object({
 
-    name              = string
     account_name      = string
-    #throughput       = string
-    max_throughput    = string
     database_name     = string
   })
 }
