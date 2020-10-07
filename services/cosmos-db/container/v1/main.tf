54eadeb56de67a2cd2287c429e8f6455bda924d2
Aditya Pavan <aditya.pavan@fdbhealth.com>
2020-10-07 11:27:54 +0530

    #4658 Cosmosdb Container creation

---
+++ services/cosmos-db/container/v1/main.tf
@@ -0,0 +1,27 @@
+
+resource "azurerm_cosmosdb_sql_container" "erx" {
+
+  #throughput          = var.service_settings.throughput
+  resource_group_name = var.context.resource_group_name
+  account_name        = var.service_settings.account_name
+  database_name       = azurerm_cosmosdb_sql_database.database.name
+  name                = var.container.name
+  partition_key_path  = var.container.partition_key_path
+
+  indexing_policy {
+    indexing_mode = "Consistent"
+
+    included_path {
+      path = "/*"
+    }
+
+    excluded_path {
+      path = "/\"_etag\"/?"
+    }
+  }
+/*
+  unique_key {
+    paths = ["/definition/idlong", "/definition/idshort"]
+  }
+*/  
+}
---
+++ services/cosmos-db/container/v1/variables.tf
@@ -0,0 +1,30 @@
+
+variable "context" {
+  type = object({
+
+    application_name    = string
+    environment_name    = string
+    resource_group_name = string
+    location            = string
+    location_suffix     = string
+
+  })
+}
+variable "service_settings" {
+  type = object({
+
+    name              = string
+    account_name      = string
+    #throughput        = string
+    max_throughput    = string
+
+  })
+}
+
+variable "container" {
+  type = object({
+    name = string
+    partition_key_path = string
+
+  })
+}
