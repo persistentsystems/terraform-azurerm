503b20167a4d48fc565c245f71853fb110c5a4fb
Santosh-Peddi <69802115+Santosh-Peddi@users.noreply.github.com>
2020-10-07 21:23:59 +0530

    API management alerts US3541 (#33)
    * Adding new folder US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541
    
    * Changes as per US3541, try as loop its failing
    
    * Changes as per US3541, try as loop its failing
    
    * Changes as per US3541, try as loop its failing
    
    * Changes as per US3541, try as loop its failing
    
    * Changes as per US3541, Reverted to single object
    
    * Changes as per US3541, updated email for alert

---
+++ services/monitor/base/v1/main.tf
@@ -0,0 +1,37 @@
+resource "azurerm_monitor_action_group" "monitor" {
+  name                = var.service_settings.name
+  resource_group_name = var.context.resource_group_name
+  short_name          = var.service_settings.short_name
+
+### For sending an email on alert
+  email_receiver {
+    name          = var.service_settings.email_title
+    email_address = var.service_settings.email_address
+  }
+
+}
+
+resource "azurerm_monitor_metric_alert" "alert" {
+  name                = var.custom_rules_settings.name
+  resource_group_name = var.context.resource_group_name
+  scopes              = var.custom_rules_settings.scopes
+  description         = var.custom_rules_settings.description
+
+  criteria {
+    metric_namespace = var.custom_rules_settings.metric_namespace
+    metric_name      = var.custom_rules_settings.metric_name
+    aggregation      = var.custom_rules_settings.aggregation
+    operator         = var.custom_rules_settings.operator
+    threshold        = var.custom_rules_settings.threshold
+
+    dimension {
+      name     = var.custom_rules_settings.dimensions_name
+      operator = var.custom_rules_settings.dimensions_operator
+      values   = var.custom_rules_settings.values
+    }
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.monitor.id
+  }
+}
---
+++ services/monitor/base/v1/variables.tf
@@ -0,0 +1,37 @@
+variable "context" {
+  type = object({
+    application_name    = string
+    environment_name    = string
+    resource_group_name = string
+    location            = string
+    location_suffix     = string
+  })
+}
+
+
+variable "service_settings" {
+  type = object({
+
+    name                = string
+    short_name          = string
+    email_title         = string
+    email_address       = string
+
+  })
+}
+
+variable "custom_rules_settings" {
+    type = object({
+        name                = string
+        scopes              = list(string)
+        description         = string
+        metric_namespace    = string
+        metric_name         = string
+        aggregation         = string
+        operator            = string
+        threshold           = string
+        dimensions_name     = string
+        dimensions_operator = string
+        values              = list(string)
+    })
+}
