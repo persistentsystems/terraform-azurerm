5bc0729c4851475545904ee921bea5e8fbd01fac
Santosh-Peddi <69802115+Santosh-Peddi@users.noreply.github.com>
2020-10-08 20:04:35 +0530

    Modifying to dynamic group US3541

--- services/monitor/base/v1/main.tf
+++ services/monitor/base/v1/main.tf
@@ -1,20 +1,3 @@
-resource "azurerm_monitor_action_group" "monitor" {
-  name                = var.service_settings.name
-  resource_group_name = var.context.resource_group_name
-  short_name          = var.service_settings.short_name
-
-### For sending an email on alert
-## Dynamic block logic
-  dynamic "email_receiver" {
-    for_each = var.email_settings
-	
-	  content {
-      name          = email_receiver.value.email_title
-      email_address = email_receiver.value.email_address
-    }
-  }
-}
-
 resource "azurerm_monitor_metric_alert" "alert" {
   name                = var.custom_rules_settings.name
   resource_group_name = var.context.resource_group_name
--- services/monitor/base/v1/variables.tf
+++ services/monitor/base/v1/variables.tf
@@ -8,23 +8,6 @@ variable "context" {
   })
 }
 
-
-variable "service_settings" {
-  type = object({
-
-    name                = string
-    short_name          = string
-
-  })
-}
-
-variable "email_settings" {
-    type = list(object({
-      email_title    = string
-      email_address = string
-    }))
-}
-
 variable "custom_rules_settings" {
     type = object({
         name                = string
