7c1aef623216ba4fd800b1cda293fc5e87bb2d21
Steve Dillon <stvdilln@yaponcha.com>
2020-10-12 14:03:50 -0400

    new v1.1 host, no changes yet

---
+++ scenarios/microservices/fn/host/premium/base/v1.1/security.tf
@@ -0,0 +1,26 @@
+data "azurerm_client_config" "current" {}
+
+module "keyvault" {
+  
+  source                    = "../../../../../../../services/keyvault/endpoint/base/v1"
+
+  context                   = var.context
+  
+  service_settings = {
+    name                    = "${var.context.application_name}-${var.context.environment_name}-${var.context.location_suffix}"
+    workspace_id            = var.service_settings.workspace_id
+    soft_delete_enabled     = var.service_settings.soft_delete_enabled
+    
+  }
+
+}
+
+
+module "terraform_admin" {
+  
+  source                    = "../../../../../../../services/keyvault/accesspolicy/templates/machine-admin/v1"
+
+  keyvault_id               = module.keyvault.id
+  application_id            = data.azurerm_client_config.current.client_id
+
+}
