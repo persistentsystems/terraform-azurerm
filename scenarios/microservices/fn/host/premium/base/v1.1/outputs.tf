586adc42da7aa6ac7b3c551e4dafabdd2c0d5f14
Steve Dillon <stvdilln@yaponcha.com>
2020-10-21 09:33:59 -0400

    remove output var

--- scenarios/microservices/fn/host/premium/base/v1.1/outputs.tf
+++ scenarios/microservices/fn/host/premium/base/v1.1/outputs.tf
@@ -4,7 +4,7 @@ output "host_settings" {
 
     plan_id                         = module.api_hosting_plan.id
     storage_account                 = module.api_hosting_plan.storage_account
-    instrumentation_key             = module.appinsights.instrumentation_key
+    #instrumentation_key             = module.appinsights.instrumentation_key
     workspace_id                    = var.service_settings.workspace_id
     deployment_storage_account_name = module.code_storage.name
     deployment_storage_container    = azurerm_storage_container.code_storage.name
