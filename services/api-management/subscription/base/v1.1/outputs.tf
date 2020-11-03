b519fadab528703316bb0a35a824c7d736b6366c
Santosh-Peddi <69802115+Santosh-Peddi@users.noreply.github.com>
2020-11-03 20:38:48 +0530

    Updated subscription_id for v1.1

--- services/api-management/subscription/base/v1.1/outputs.tf
+++ services/api-management/subscription/base/v1.1/outputs.tf
@@ -1,6 +1,9 @@
 output "id" {
   value = azurerm_api_management_subscription.subscription.id
 }
+
+/*
 output "subscription_id" {
   value = azurerm_api_management_subscription.subscription.subscription_id
 }
+*/
