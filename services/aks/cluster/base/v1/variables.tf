6add9da6b94f8bbbe35ab8e67f6fa543085d769a
Steve Dillon <stvdilln@yaponcha.com>
2021-01-04 12:24:43 -0500

    remove older version , collapse v1.2 into v1

--- services/aks/cluster/base/v1/variables.tf
+++ services/aks/cluster/base/v1/variables.tf
@@ -9,14 +9,30 @@ variable "context" {
 
   })
 }
-# node_size = "Standard_D2_v2"
+# There are a lot of parameters, and some i'm omitting to keep 
+# the parameter list manageable.  There is an open ticket in github
+# Aug 2020, that is trying to address having default values for 
+# objects.  Waiting for this before adding even more options.
 variable "service_settings" {
   type = object({
 
     name                = string
     resource_group_name = string
     node_count          = number
+    node_min_count      = number
+    node_max_count      = number 
     node_size           = string
+    rbac_enabled        = bool 
+    enable_pod_security_policy = bool 
+    api_server_authorized_ip_ranges = list(string)
 
+  })
+}
+variable "observability_settings" {
+  type = object({
+    instrumentation_key       = string
+    workspace_id              = string
+    storage_account           = string 
+    retention_days            = number
   })
 }
