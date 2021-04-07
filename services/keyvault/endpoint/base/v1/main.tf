b1506890895e2b9de5b9665b47cf80506f1a0c5a
Steve Dillon <stvdilln@yaponcha.com>
2021-04-07 10:48:40 -0400

    Set Keyvault permissions for current user. Otherwise the accoun running terraform cannot change any keys.  Maybe we should force user/ or default this. May not be the last iteration of these permissions.

--- services/keyvault/endpoint/base/v1/main.tf
+++ services/keyvault/endpoint/base/v1/main.tf
@@ -6,6 +6,30 @@ resource "random_string" "random" {
   lower = true
   upper = false
 }
+resource "azurerm_key_vault_access_policy" "policy" {
+  key_vault_id = azurerm_key_vault.keyvault.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+
+
+  certificate_permissions = [
+    "Get", "Create", "List", "Delete", "Update"
+  ]
+
+  key_permissions = [
+    "get", "create", "list", "delete"
+  ]
+
+  secret_permissions = [
+    "get", "list" , "set", "delete", "purge", "restore"
+  ]
+
+  storage_permissions = [
+    "get", "set", "list", "delete"
+  ]
+
+}
 
 resource "azurerm_key_vault" "keyvault" {
   name                        = "${var.service_settings.name}-${random_string.random.result}"
@@ -18,27 +42,6 @@ resource "azurerm_key_vault" "keyvault" {
   enabled_for_disk_encryption = true
 
   sku_name = "standard"
-  # access_policy {
-  #   tenant_id = data.azurerm_client_config.current.tenant_id
-  #   object_id = data.azurerm_client_config.current.object_id
-
-  #   certificate_permissions = [
-  #     "Get", "Create", "List", "Delete", "Update"
-  #   ]
-
-  #   key_permissions = [
-  #     "get", "create", "list", "delete"
-  #   ]
-
-  #   secret_permissions = [
-  #     #"Delete", "Get", "List","Set" 
-  #     "get", "list" , "set", "delete", "purge", "restore"
-  #   ]
-
-  #   storage_permissions = [
-  #     "get", "set", "list", "delete"
-  #   ]
-  # }
 
   network_acls {
     /*
