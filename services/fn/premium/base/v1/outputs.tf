2a3929aecc06086ea38f476c7ec17e985b42a3b2
Steve Dillon <stvdilln@yaponcha.com>
2021-04-13 16:23:35 -0400

    directly fetch API function keys.  This resource proably didn't exist when this was written.

--- services/fn/premium/base/v1/outputs.tf
+++ services/fn/premium/base/v1/outputs.tf
@@ -1,5 +1,8 @@
-output "function_key" {
-    value = "${lookup(azurerm_template_deployment.azfn_function_key.outputs, "functionkey")}"
+#output "function_key" {
+#    value = "${lookup(azurerm_template_deployment.azfn_function_key.outputs, "functionkey")}"
+#}
+output function_key {
+    value = data.azurerm_function_app_host_keys.azfn_function_key.default_function_key
 }
 output "hostname" {
     value = azurerm_function_app.function_app.default_hostname
