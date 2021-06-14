e37989508a26c0b8e764e0a2030c4c4b46624620
Santosh-Peddi <69802115+Santosh-Peddi@users.noreply.github.com>
2021-06-14 19:05:06 +0530

    US8148: DR changes

--- services/frontdoor/endpoint/base/v1/main.tf
+++ services/frontdoor/endpoint/base/v1/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_frontdoor" "frontdoor" {
   frontend_endpoint {
     name                                    = "DefaultEndpoint"
     host_name                               = "${var.service_settings.name}.azurefd.net"
-    custom_https_provisioning_enabled       = false
+  #  custom_https_provisioning_enabled       = false
     web_application_firewall_policy_link_id = var.service_settings.web_application_firewall_policy_link_id
   }
 
@@ -69,4 +69,9 @@ resource "azurerm_frontdoor" "frontdoor" {
     health_probe_name   = var.backend_settings.name
 
   }
+}
+
+resource "azurerm_frontdoor_custom_https_configuration" "custom_https_0" {
+  frontend_endpoint_id              = azurerm_frontdoor.frontdoor.frontend_endpoints["DefaultEndpoint"]
+  custom_https_provisioning_enabled = false
 }
