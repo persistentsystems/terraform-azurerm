bf1aca40e81bafc5218ff06d1cd13ccace1cb181
Santosh-Peddi <69802115+Santosh-Peddi@users.noreply.github.com>
2021-08-06 16:50:25 +0530

    Removed staging code

--- scenarios/microservices/fn/http/premium/base/v1/main.tf
+++ scenarios/microservices/fn/http/premium/base/v1/main.tf
@@ -20,7 +20,7 @@ locals {
 
 module "api_fn" {
   
-  source                  = "../../../../../../../services/fn/premium/staged/v1"
+  source                  = "../../../../../../../services/fn/premium/base/v1"
   
   context                = var.context
   service_settings       = local.merged_service_settings
