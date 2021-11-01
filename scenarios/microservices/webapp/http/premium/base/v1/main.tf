8c10da33e07d0e38d73cf24c25b15450239328d2
Murali <murali_imadabattuni@persistent.com>
2021-11-01 22:12:03 +0530

    created a admin-ui http microservices

---
+++ scenarios/microservices/webapp/http/premium/base/v1/main.tf
@@ -0,0 +1,39 @@
+
+
+locals {
+
+  required_settings = {
+      "WEBSITE_RUN_FROM_PACKAGE"          = local.code_drop_url,
+  }
+  combined_settings = merge(local.required_settings, var.service_settings.app_settings)
+
+  merged_service_settings = {
+    name                = var.service_settings.name
+    runtime_version     = var.service_settings.runtime_version
+    runtime_type        = var.service_settings.runtime_type
+    app_settings        = local.combined_settings
+    plan_id             = var.host_settings.plan_id
+    storage_account     = var.host_settings.storage_account
+    storage_account_id  = var.observability_settings.storage_account_id
+  }
+}
+
+module "api_webapp" {
+  
+  source                  = "../../../../../../../services/admin-ui/base/v1"
+  
+  context                = var.context
+  service_settings       = local.merged_service_settings
+  observability_settings = var.observability_settings  
+  tags                  = var.tags
+  
+}
+
+module "identity_access_policy" {
+  
+  source                    = "../../../../../../../services/keyvault/accesspolicy/templates/managed-identity-reader/v1"
+
+  keyvault_id               = var.keyvault_id
+  object_id                 = module.api_webapp.identity[0].principal_id
+
+}
---
+++ scenarios/microservices/webapp/http/premium/base/v1/variables.tf
@@ -0,0 +1,54 @@
+variable "context" {
+  type = object({
+
+    application_name            = string
+    environment_name            = string
+    resource_group_name = string
+    location            = string
+    location_suffix     = string
+
+  })
+}
+variable "host_settings" {
+  type = object({
+
+    plan_id                         = string
+    storage_account                 = object({
+        connection_string = string
+        name = string
+        access_key = string
+    })
+    deployment_storage_account_name = string
+    deployment_storage_container    = string
+    deployment_package_sas          = string
+
+  })
+}
+variable keyvault_id {
+    type = string
+}
+variable "service_settings" {
+  type = object({
+
+    name              = string
+    app_settings      = map(string)
+    package_filename  = string
+
+  })
+}
+
+variable "observability_settings" {
+  type = object({
+
+    instrumentation_key = string
+    workspace_id        = string
+    storage_account     = string 
+    retention_in_days   = number
+    storage_account_id  = string
+  })
+}
+variable tags {
+
+  type = map
+    
+}
