534a33b141c68c33ad24c4ce174c961f4575c223
Murali <murali_imadabattuni@persistent.com>
2021-11-02 11:59:06 +0530

    changes in adminui microservices

--- scenarios/microservices/webapp/http/premium/base/v1/outputs.tf
+++ scenarios/microservices/webapp/http/premium/base/v1/outputs.tf
@@ -1,15 +1,15 @@
 output "function_name" {
     value = module.api_webapp.name
 }
-output "function_key" {
-    value = module.api_webapp.function_key
-}
+# output "function_key" {
+#     value = module.api_webapp.function_key
+# }
 output "identity" {
     value = module.api_webapp.identity
 }
-output "hostname" {
-    value = module.api_webapp.hostname
-}
+# output "hostname" {
+#     value = module.api_webapp.hostname
+# }
 output "id" {
     value = module.api_webapp.id
 }
