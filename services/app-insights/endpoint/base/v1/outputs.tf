35593f4b4291c742ac90a0222678f0b3f3fee9b6
Murali <murali_imadabattuni@persistent.com>
2021-11-17 16:53:55 +0530

    added output block for app-insights conn_string

--- services/app-insights/endpoint/base/v1/outputs.tf
+++ services/app-insights/endpoint/base/v1/outputs.tf
@@ -1,11 +1,9 @@
 output "instrumentation_key" {
   value = azurerm_application_insights.main.instrumentation_key
 }
-
 output "app_id" {
   value = azurerm_application_insights.main.app_id
 }
-
 output "id" {
   value = azurerm_application_insights.main.id
 }
