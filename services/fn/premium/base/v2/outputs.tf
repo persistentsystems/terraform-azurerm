c466b3dab41ba9bab431f3f79deccbdefd21d99d
Murali <murali_imadabattuni@persistent.com>
2022-02-08 08:15:20 +0530

    created new fn version v2

---
+++ services/fn/premium/base/v2/outputs.tf
@@ -0,0 +1,21 @@
+#output "function_key" {
+#    value = "${lookup(azurerm_template_deployment.azfn_function_key.outputs, "functionkey")}"
+#}
+output function_key {
+    value = data.azurerm_function_app_host_keys.azfn_function_key.default_function_key
+}
+output "hostname" {
+    value = azurerm_function_app.function_app.default_hostname
+}
+output "name" {
+    value = azurerm_function_app.function_app.name
+}
+output "identity" {
+    value = azurerm_function_app.function_app.identity
+}
+output "id" {
+  value = azurerm_function_app.function_app.id
+}
+output "function_app_id" {
+  value = azurerm_function_app.function_app.id
+}
