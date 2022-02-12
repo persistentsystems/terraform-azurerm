1f0a2ac2166311cbce97ee67c6f1faabcd89350f
Santosh-Peddi <69802115+Santosh-Peddi@users.noreply.github.com>
2022-02-12 12:54:28 +0530

    Added code for App keys read

--- services/fn/premium/base/v2/outputs.tf
+++ services/fn/premium/base/v2/outputs.tf
@@ -1,9 +1,9 @@
 #output "function_key" {
 #    value = "${lookup(azurerm_template_deployment.azfn_function_key.outputs, "functionkey")}"
 #}
-# output function_key {
-#     value = data.azurerm_function_app_host_keys.azfn_function_keyv2.default_function_key
-# }
+output function_key {
+    value = data.azurerm_function_app_host_keys.azfn_function_keyv2.default_function_key
+}
 output "function_name" {
     value = azurerm_function_app.function_appv2.name
 }
