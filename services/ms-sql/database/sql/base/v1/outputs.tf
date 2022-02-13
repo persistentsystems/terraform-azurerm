3f5481ba1005cedc9880152b9df166104da76396
Murali <murali_imadabattuni@persistent.com>
2022-02-13 16:23:06 +0530

    outputting connection string

--- services/ms-sql/database/sql/base/v1/outputs.tf
+++ services/ms-sql/database/sql/base/v1/outputs.tf
@@ -4,6 +4,6 @@ output "name" {
 output "id" {
     value = azurerm_mssql_database.mssql-db.id
 }
-output "connection_string" {
-    value = azurerm_mssql_database.mssql-db.connection_string
-}
+# output "connection_string" {
+#     value = azurerm_mssql_database.mssql-db.connection_string
+# }
