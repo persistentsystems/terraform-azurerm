7f2000994ad1b98f6e39b9aab536a1b617561106
Murali <murali_imadabattuni@persistent.com>
2022-02-13 09:34:10 +0530

    added randaom string

--- services/ms-sql/sql-server/v1/main.tf
+++ services/ms-sql/sql-server/v1/main.tf
@@ -6,8 +6,15 @@ locals {
 
     final_tags = merge (local.default_tags, var.tags ) 
 }
+resource "random_string" "random" {
+  length = 8
+  special = false
+  lower = true
+  upper = false
+}
+
 resource "azurerm_mssql_server" "mssql" {
-  name                         = "var.service_settings.name"
+  name                         = "${var.service_settings.name}-${random_string.random.result}"
   resource_group_name          = var.context.resource_group_name
   location                     = var.context.location
   version                      = var.service_settings.version
