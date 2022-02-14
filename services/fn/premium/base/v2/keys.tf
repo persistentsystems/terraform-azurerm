1fc316d2a46cf86b2d810fe74b2c1f2d11b52943
Murali <murali_imadabattuni@persistent.com>
2022-02-14 21:29:46 +0530

    set the sleep time as 120secs for app keys

--- services/fn/premium/base/v2/keys.tf
+++ services/fn/premium/base/v2/keys.tf
@@ -1,5 +1,17 @@
-# data azurerm_function_app_host_keys azfn_function_keyv2 {
-#   depends_on = [azurerm_function_app.function_appv2]
-#   name       = azurerm_function_app.function_appv2.name
-#   resource_group_name    = var.context.resource_group_name 
-# }
+data azurerm_function_app_host_keys azfn_function_keyv2 {
+  depends_on = [azurerm_function_app.function_appv2]
+  name       = azurerm_function_app.function_appv2.name
+  resource_group_name    = var.context.resource_group_name 
+}
+
+
+resource "time_sleep" "wait_120_seconds" {
+  depends_on = [data.azurerm_function_app_host_keys.azfn_function_keyv2]
+
+  create_duration = "120s"
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_120_seconds]
+}
