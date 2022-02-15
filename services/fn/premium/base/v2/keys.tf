19fb87f5e0438f1ed045744e4531bef0dd02a198
Murali <murali_imadabattuni@persistent.com>
2022-02-15 12:14:43 +0530

    commented the sleep for fn keys

--- services/fn/premium/base/v2/keys.tf
+++ services/fn/premium/base/v2/keys.tf
@@ -4,13 +4,13 @@ data azurerm_function_app_host_keys azfn_function_keyv2 {
   resource_group_name    = var.context.resource_group_name 
 }
 
-resource "time_sleep" "wait_30_seconds_for_keys" {
-  depends_on = [azurerm_function_app_host_keys.azfn_function_keyv2]
+# resource "time_sleep" "wait_30_seconds_for_keys" {
+#   depends_on = [azurerm_function_app_host_keys.azfn_function_keyv2]
 
-  create_duration = "30s"
-}
+#   create_duration = "30s"
+# }
 
-# This resource will create (at least) 30 seconds after null_resource.previous
-resource "null_resource" "wait_30_secs" {
-  depends_on = [time_sleep.wait_30_seconds_for_keys]
-}
+# # This resource will create (at least) 30 seconds after null_resource.previous
+# resource "null_resource" "wait_30_secs" {
+#   depends_on = [time_sleep.wait_30_seconds_for_keys]
+# }
