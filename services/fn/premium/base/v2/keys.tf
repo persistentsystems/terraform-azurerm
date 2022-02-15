84a0abdfd2a548558935d0e7491bfc2f810df62e
Santosh-Peddi <69802115+Santosh-Peddi@users.noreply.github.com>
2022-02-15 10:19:40 +0530

    Added wait time

--- services/fn/premium/base/v2/keys.tf
+++ services/fn/premium/base/v2/keys.tf
@@ -3,3 +3,14 @@ data azurerm_function_app_host_keys azfn_function_keyv2 {
   name       = azurerm_function_app.function_appv2.name
   resource_group_name    = var.context.resource_group_name 
 }
+
+resource "time_sleep" "wait_30_seconds_for_keys" {
+  depends_on = [azurerm_function_app_host_keys.azfn_function_keyv2]
+
+  create_duration = "30s"
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "wait_30_secs" {
+  depends_on = [time_sleep.wait_30_seconds_for_keys]
+}
