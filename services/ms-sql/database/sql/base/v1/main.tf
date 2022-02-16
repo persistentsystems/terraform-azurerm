f0864e030c6d26732ce5111f27f4898447a48933
Murali <murali_imadabattuni@persistent.com>
2022-02-16 15:12:23 +0530

    created the mssql-DB

--- services/ms-sql/database/sql/base/v1/main.tf
+++ services/ms-sql/database/sql/base/v1/main.tf
@@ -8,23 +8,21 @@ locals {
 }
 
 
-resource "azurerm_mssql_elasticpool" "mssql-db" {
-  name                = var.service_settings.name
-  resource_group_name = var.context.resource_group_name
-  location            = var.context.location
-  server_name         = var.service_settings.server_name
-  license_type        = var.service_settings.license_type
-  max_size_gb         = var.service_settings.max_size_gb   
-  zone_redundant      = var.service_settings.zone_redundant
-  sku  {
-    name         = var.service_settings.sku_name
-    tier         = var.service_settings.tier
-    family       = var.service_settings.family
-    capacity     = var.service_settings.capacity
-  } 
-  per_database_settings {
-    min_capacity = var.service_settings.min_capacity
-    max_capacity = var.service_settings.max_capacity
+resource "azurerm_mssql_database" "mssql-db" {
+  name           = var.service_settings.name
+  server_id      = var.service_settings.server_id
+  collation      = var.service_settings.collation
+  license_type   = var.service_settings.license_type
+  max_size_gb    = var.service_settings.max_size_gb
+  read_scale     = var.service_settings.read_scale
+  sku_name       = var.service_settings.sku_name
+  zone_redundant = var.service_settings.zone_redundant
+  auto_pause_delay_in_minutes = var.service_settings.auto_pause_delay_in_minutes
+
+  extended_auditing_policy {
+    storage_endpoint                        = var.service_settings.storage_endpoint
+    storage_account_access_key              = var.service_settings.storage_account_access_key
+    retention_in_days                       = var.service_settings.retention_in_days
   }
   tags                      = local.final_tags
 
--- services/ms-sql/database/sql/base/v1/variables.tf
+++ services/ms-sql/database/sql/base/v1/variables.tf
@@ -12,18 +12,18 @@ variable "context" {
 }
 variable "service_settings" {
   type = object({
-
-    name              = string
-    server_name       = string    
-    license_type      = string
-    max_size_gb       = string
-    sku_name          = string
-    tier              = string
-    family            = string
-    capacity          = string
-    zone_redundant    = bool
-    min_capacity      = string
-    max_capacity      = string
+    name                      = string
+    server_id                 = string
+    collation                 = string
+    license_type              = string
+    max_size_gb               = string
+    read_scale                = bool
+    sku_name                  = string
+    zone_redundant            = bool
+    storage_endpoint          = string
+    storage_account_access_key = string
+    retention_in_days          = string
+    auto_pause_delay_in_minutes = string
 
   })
 }
