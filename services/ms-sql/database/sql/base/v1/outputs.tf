8877aeebfadc876e11ba93012c955871e10feda8
Murali <murali_imadabattuni@persistent.com>
2022-02-16 13:31:43 +0530

    changed the database type

--- services/ms-sql/database/sql/base/v1/outputs.tf
+++ services/ms-sql/database/sql/base/v1/outputs.tf
@@ -1,8 +1,8 @@
 output "name" {
-    value = azurerm_mssql_database.mssql-db.name
+    value = azurerm_mssql_elasticpool.mssql-db.name
 }
 output "id" {
-    value = azurerm_mssql_database.mssql-db.id
+    value = azurerm_mssql_elasticpool.mssql-db.id
 }
 # output "connection_string" {
 #     value = azurerm_mssql_database.mssql-db.connection_string
