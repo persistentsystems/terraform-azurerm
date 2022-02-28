204575fea2526e4c5d397034b58a629f50c1b9eb
Murali.Imadabattuni <Murali.Imadabattuni@fdbhealth.com>
2022-02-28 20:40:04 +0530

    outputting postgres creds

--- services/postgres-db/endpoint/base/v1/outputs.tf
+++ services/postgres-db/endpoint/base/v1/outputs.tf
@@ -9,3 +9,9 @@ output "name" {
 output "fqdn" {
     value = azurerm_postgresql_flexible_server.postgres_server.fqdn
 }
+output "postgres_admin_login" {
+  value = azurerm_postgresql_flexible_server.postgres_server.administrator_login
+}
+output "postgres_admin_password" {
+  value = azurerm_postgresql_flexible_server.postgres_server.administrator_password
+}
