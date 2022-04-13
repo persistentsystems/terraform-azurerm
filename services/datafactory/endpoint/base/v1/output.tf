5f4d74d1679aa85a8c1efa9a022a81d03b700036
Murali.Imadabattuni <Murali.Imadabattuni@fdbhealth.com>
2022-04-13 08:43:01 +0000

    added system identity to DF and created role-assignments

--- services/datafactory/endpoint/base/v1/output.tf
+++ services/datafactory/endpoint/base/v1/output.tf
@@ -7,3 +7,6 @@ output "name" {
   value       = azurerm_data_factory.datafactory.name
   description = "The generated ID for the ADF created"
 }
+output "principal_id" {
+  value = azurerm_data_factory.datafactory.identity.0.principal_id
+}
