9e9f0f90bc82c17ee73193a05d2eeec97703254c
Murali.Imadabattuni <Murali.Imadabattuni@fdbhealth.com>
2022-06-09 10:49:50 +0530

    observability_settings for adminui service plan

---
+++ services/app-service/plan/premium/base/v2/mointor.tf
@@ -0,0 +1,68 @@
+data "azurerm_monitor_diagnostic_categories" "diagnostic_categories" {
+  resource_id = azurerm_app_service_plan.app_service_plan.id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "adminui_plan_diagnostic_setting" {
+
+  name                        = "adminui-appserviceplan-logs"
+  target_resource_id          = azurerm_app_service_plan.app_service_plan.id
+  log_analytics_workspace_id  = var.observability_settings.workspace_id
+
+  dynamic log {
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories.logs
+    content {
+      category = log.value
+      enabled  = true
+
+      retention_policy {
+        enabled = true
+        days = var.observability_settings.retention_days
+      }
+    }
+  }
+
+  dynamic metric {
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories.metrics 
+    content {
+      category = metric.value
+
+      retention_policy {
+        enabled = true
+        days = var.observability_settings.retention_days
+      }
+    }
+  }
+
+
+
+}
+
+resource "azurerm_monitor_diagnostic_setting" "adminui_plan_log_setting" {
+
+  name                        = "adminui-appserviceplan-storage"
+  target_resource_id          = azurerm_app_service_plan.app_service_plan.id
+  storage_account_id          = var.observability_settings.storage_account
+
+  dynamic log {
+    for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories.logs
+    content {
+      category = log.value
+      enabled  = true
+
+      retention_policy {
+        enabled = true
+        days = 0
+      }
+    }
+  }
+  metric {
+      category = "AllMetrics"
+      enabled  = true 
+
+      retention_policy {
+        enabled = true
+        days = 0
+      }
+  }
+
+}
--- services/app-service/plan/premium/base/v2/variables.tf
+++ services/app-service/plan/premium/base/v2/variables.tf
@@ -24,3 +24,14 @@ variable tags {
   type = map
     
 }
+
+variable "observability_settings" {
+  type = object({
+
+    #instrumentation_key = string
+    workspace_id        = string
+    storage_account     = string 
+    retention_days      = number
+    #storage_account_id  = string
+  })
+}
