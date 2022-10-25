a0ad5136e3a40803175a949072b5cf6a0259856e
Murali.Imadabattuni <Murali.Imadabattuni@fdbhealth.com>
2022-10-25 14:17:14 +0530

    created new version for key vault v2

---
+++ services/keyvault/endpoint/base/v2/monitor.tf
@@ -0,0 +1,66 @@
+# data "azurerm_monitor_diagnostic_categories" "diagnostic_categories" {
+#   resource_id = azurerm_key_vault.keyvault.id
+# }
+
+# resource "azurerm_monitor_diagnostic_setting" "keyvault_diagnostic_setting" {
+#   name                        = "${var.service_settings.name}-keyvault-logs"
+#   target_resource_id          = azurerm_key_vault.keyvault.id
+#   log_analytics_workspace_id  = var.observability_settings.workspace_id
+
+#   dynamic log {
+#     for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories.logs
+#     content {
+#       category = log.value
+#       enabled  = true
+
+#       retention_policy {
+#         enabled = true
+#         days = var.observability_settings.retention_days
+#       }
+#     }
+#   }
+
+#   dynamic metric {
+#     for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories.metrics 
+#     content {
+#       category = metric.value
+
+#       retention_policy {
+#         enabled = true
+#         days = var.observability_settings.retention_days
+#       }
+#     }
+#   }
+
+
+
+# }
+
+# resource "azurerm_monitor_diagnostic_setting" "keyvault_log_setting" {
+#   name                        = "${var.service_settings.name}-keyvault-storage"
+#   target_resource_id          = azurerm_key_vault.keyvault.id
+#   storage_account_id          = var.observability_settings.storage_account
+
+#   dynamic log {
+#     for_each = data.azurerm_monitor_diagnostic_categories.diagnostic_categories.logs
+#     content {
+#       category = log.value
+#       enabled  = true
+
+#       retention_policy {
+#         enabled = true
+#         days = 0
+#       }
+#     }
+#   }
+
+#   metric {
+#     category = "AllMetrics"
+#     enabled  = false
+
+#     retention_policy {
+#       enabled = false
+#       days = 0
+#     }
+#   }
+# }
---
+++ services/keyvault/endpoint/base/v2/variables.tf
@@ -0,0 +1,43 @@
+  
+variable "context" {
+  type = object({
+
+    application_name = string
+    environment_name = string
+    resource_group_name = string
+    location = string
+    location_suffix = string
+
+  })
+}
+
+variable "service_settings" {
+  type = object({
+
+    name                = string
+
+  })
+}
+
+# variable "observability_settings" {
+#   type = object({
+
+#     instrumentation_key = string
+#     # log analytics
+#     workspace_id        = string
+#     storage_account     = string 
+#     retention_days   = number
+    
+
+#   })
+# }
+variable "soft_delete_retention_days"{
+  type = number
+  default = 90
+}
+
+variable tags {
+
+  type = map
+    
+}
