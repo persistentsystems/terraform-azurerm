fd2821fe9ef1532bc78c595a36a6975325a71055
Murali.Imadabattuni <Murali.Imadabattuni@fdbhealth.com>
2023-03-07 11:09:01 +0530

    created network rules for storage  account

---
+++ services/network/storage-network-rules/main.tf
@@ -0,0 +1,7 @@
+resource "azurerm_storage_account_network_rules" "example" {
+  storage_account_id         = var.service_settings.storage_account_id
+  default_action             = var.service_settings.default_action
+  ip_rules                   = var.service_settings.ip_rules
+  virtual_network_subnet_ids = var.service_settings.virtual_network_subnet_ids
+  bypass                     = ["AzureServices"]
+}
